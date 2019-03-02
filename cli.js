c9acf17893b741172216cf5d4b8869185d2b9bd9
pablopunk <pablovarela182@gmail.com>
2019-03-02 19:57:08 +0100

    Remove unused index and fix decimals

--- cli.js
+++ cli.js
@@ -1,14 +1,12 @@
 #!/usr/bin/env node
 const stats = require('pc-stats')
 
-//   free: '13.75', total: '34.36', unit: 'GB' }
-//               total        used        free      shared  buff/cache   available
-// Mem:           875M         80M        239M         44M        555M        695M
-// Swap:           99M          0B         99M
-
 stats().then(({ ram }) => {
+  const { total, unit, free } = ram
+  const used = (total-free).toFixed(2)
+
   console.log(`
     \t\ttotal\t\tused\t\tfree
-Mem:\t\t${ram.total}${ram.unit}\t\t${ram.total-ram.free}${ram.unit}\t\t${ram.free}${ram.unit}
+Mem:\t\t${total}${unit}\t\t${used}${unit}\t\t${free}${unit}
   `)
 })
